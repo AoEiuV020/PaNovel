084aa4b62a0412d9e6574c290c0ad5a2ba6123fc
AoEiuV020 <aoeiuv020@gmail.com>
2017-10-09 13:47:00 +0800

    解决两个警告
    kotlin很容易警告，
    java方法返回不知是否可空就警告，
    能私有的东西也警告，

--- app/src/main/java/cc/aoeiuv020/panovel/App.kt
+++ app/src/main/java/cc/aoeiuv020/panovel/App.kt
@@ -4,18 +4,20 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import cc.aoeiuv020.panovel.api.paNovel
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
 /**
  *
  * Created by AoEiuV020 on 2017.10.03-17:04:22.
  */
+@Suppress("MemberVisibilityCanPrivate")
 class App : Application() {
     companion object {
         @SuppressLint("StaticFieldLeak")
         lateinit var ctx: Context
-        val gsonBuilder = GsonBuilder().paNovel()
-        val gson = gsonBuilder.create()
+        val gsonBuilder: GsonBuilder = GsonBuilder().paNovel()
+        val gson: Gson = gsonBuilder.create()
     }
 
     override fun onCreate() {
