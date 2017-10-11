d431c1bd28a8d360edf22544cca9f0e1f63a068e
AoEiuV020 <aoeiuv020@gmail.com>
2017-10-11 11:05:24 +0800

    通知改成不可点击
    那些个activity必须传入intent才能用，

--- app/src/main/java/cc/aoeiuv020/panovel/ui/ext.kt
+++ app/src/main/java/cc/aoeiuv020/panovel/ui/ext.kt
@@ -3,10 +3,8 @@
 package cc.aoeiuv020.panovel.ui
 
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.app.ProgressDialog
 import android.content.Context
-import android.content.Intent
 import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AlertDialog
 import android.view.View
@@ -53,14 +51,10 @@ fun Context.alertColorPicker(initial: Int, callback: (color: Int) -> Unit) = Col
         .build().show()
 
 fun Context.notify(id: Int, text: String? = null, title: String? = null) {
-    val pi = PendingIntent.getActivity(this, 1, Intent(this, this.javaClass)
-            .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
-            PendingIntent.FLAG_UPDATE_CURRENT)
     val nb = NotificationCompat.Builder(this)
             .setContentTitle(title)
             .setContentText(text)
             .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentIntent(pi)
             .setAutoCancel(true)
     (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).notify(id, nb.build())
 }
