4a133d5b064a5754d29a10b94c4b92b0fb0420a4
AoEiuV020 <aoeiuv020@gmail.com>
2017-10-11 18:39:40 +0800

    不用kotlin的View.findView拓展
    这个不缓存的，

--- app/src/main/java/cc/aoeiuv020/panovel/text/NovelTextViewHolder.kt
+++ app/src/main/java/cc/aoeiuv020/panovel/text/NovelTextViewHolder.kt
@@ -3,6 +3,9 @@ package cc.aoeiuv020.panovel.text
 import android.annotation.SuppressLint
 import android.view.MotionEvent
 import android.view.View
+import android.widget.ListView
+import android.widget.ProgressBar
+import android.widget.TextView
 import cc.aoeiuv020.panovel.IView
 import cc.aoeiuv020.panovel.R
 import cc.aoeiuv020.panovel.api.NovelChapter
@@ -18,51 +21,55 @@ import org.jetbrains.anko.dip
 class NovelTextViewHolder(private val ctx: NovelTextActivity, presenter: NovelTextPresenter) : IView, AnkoLogger {
     val itemView: View = View.inflate(ctx, R.layout.novel_text_page_item, null)
     private val presenter = presenter.subPresenter(this)
-    private val headerView = View.inflate(ctx, R.layout.novel_text_header, null).apply {
-        chapterNameTextView.setTextColor(Settings.textColor)
-    }
+    private val headerView = View.inflate(ctx, R.layout.novel_text_header, null)
+    private val chapterNameTextView: TextView
+    private val textListView: ListView
+    private val progressBar: ProgressBar
+
     private val textListAdapter = NovelTextListAdapter(ctx)
     private var paragraphSpacing = Settings.paragraphSpacing
     private var textProgress: Int? = null
 
     init {
-        itemView.textListView.apply {
-            addHeaderView(headerView, null, false)
-            dividerHeight = ctx.dip(paragraphSpacing)
-            setOnTouchListener(object : View.OnTouchListener {
-                private var previousAction: Int = MotionEvent.ACTION_UP
-                @SuppressLint("ClickableViewAccessibility")
-                override fun onTouch(v: View?, event: MotionEvent): Boolean {
-                    if (previousAction == MotionEvent.ACTION_DOWN
-                            && event.action == MotionEvent.ACTION_UP) {
-                        ctx.toggle()
-                    }
-                    previousAction = event.action
-                    return false
+        textListView = itemView.textListView
+        textListView.addHeaderView(headerView, null, false)
+        textListView.dividerHeight = ctx.dip(paragraphSpacing)
+        // 这有个警告禁用不了，只能这样无意义的强转一下，
+        // Custom view `ListView` has setOnTouchListener called on it but does not override performClick
+        (textListView as View).setOnTouchListener(object : View.OnTouchListener {
+            private var previousAction: Int = MotionEvent.ACTION_UP
+            @SuppressLint("ClickableViewAccessibility")
+            override fun onTouch(v: View?, event: MotionEvent): Boolean {
+                if (previousAction == MotionEvent.ACTION_DOWN
+                        && event.action == MotionEvent.ACTION_UP) {
+                    ctx.toggle()
                 }
-            })
-        }
+                previousAction = event.action
+                return false
+            }
+        })
+        chapterNameTextView = headerView.chapterNameTextView
+        chapterNameTextView.setTextColor(Settings.textColor)
+        progressBar = itemView.progressBar
     }
 
     fun apply(chapter: NovelChapter) {
-        itemView.progressBar.show()
+        progressBar.show()
         headerView.chapterNameTextView.text = chapter.name
-        itemView.textListView.adapter = null
+        textListView.adapter = null
         presenter.requestNovelText(chapter)
     }
 
     fun showText(novelText: NovelText) {
         textListAdapter.setNovelText(novelText)
-        itemView.textListView.apply {
+        textListView.apply {
             adapter = textListAdapter
             textProgress?.let {
-                post {
-                    setSelection(it)
-                }
+                post { setSelection(it) }
                 textProgress = null
             }
         }
-        itemView.progressBar.hide()
+        progressBar.hide()
     }
 
     fun showError(message: String, e: Throwable) {
