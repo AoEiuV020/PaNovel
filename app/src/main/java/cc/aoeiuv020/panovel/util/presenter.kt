6a93afbe10501b58db0557df459a95e20a85a264
AoEiuV020 <aoeiuv020@gmail.com>
2017-10-21 22:16:18 +0800

    线程复用，4线程

--- app/src/main/java/cc/aoeiuv020/panovel/util/presenter.kt
+++ app/src/main/java/cc/aoeiuv020/panovel/util/presenter.kt
@@ -3,12 +3,37 @@ package cc.aoeiuv020.panovel.util
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.Executor
+import java.util.concurrent.LinkedBlockingDeque
 
 /**
  *
  * Created by AoEiuV020 on 2017.10.02-21:42:59.
  */
 
+private val asyncExecutor = object : Executor {
+    val tasks = LinkedBlockingDeque<Runnable>()
+    val threads = List(4) {
+        newThread(it)
+    }
+
+    init {
+        threads.forEach {
+            it.start()
+        }
+    }
+
+    fun newThread(index: Int) = Thread({
+        while (true) {
+            tasks.take().run()
+        }
+    }, "async-$index")
+
+    override fun execute(command: Runnable) {
+        tasks.push(command)
+    }
+}
+
 fun <T : Any?> Observable<T>.async(): Observable<T> = this
-        .subscribeOn(Schedulers.from { Thread(it).start() })
+        .subscribeOn(Schedulers.from(asyncExecutor))
         .observeOn(AndroidSchedulers.mainThread())
