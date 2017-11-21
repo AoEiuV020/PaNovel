1246dfedf8f70a6686fc51fdb4bea59207f80d0b
AoEiuV020 <aoeiuv020@gmail.com>
2017-11-21 17:18:14 +0800

    删除小说阅读页面之前残留的headerView

--- app/src/main/java/cc/aoeiuv020/panovel/text/NovelTextViewHolder.kt
+++ app/src/main/java/cc/aoeiuv020/panovel/text/NovelTextViewHolder.kt
@@ -2,12 +2,10 @@ package cc.aoeiuv020.panovel.text
 
 import android.annotation.SuppressLint
 import android.support.v7.widget.LinearLayoutManager
-import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
-import android.widget.TextView
 import cc.aoeiuv020.panovel.IView
 import cc.aoeiuv020.panovel.R
 import cc.aoeiuv020.panovel.api.NovelChapter
@@ -15,7 +13,6 @@ import cc.aoeiuv020.panovel.api.NovelText
 import cc.aoeiuv020.panovel.local.Settings
 import cc.aoeiuv020.panovel.util.hide
 import cc.aoeiuv020.panovel.util.show
-import kotlinx.android.synthetic.main.novel_text_header.view.*
 import kotlinx.android.synthetic.main.novel_text_page_item.view.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.debug
@@ -23,8 +20,6 @@ import org.jetbrains.anko.debug
 class NovelTextViewHolder(private val ctx: NovelTextActivity, private val presenter: NovelTextPresenter.NTPresenter) : IView, AnkoLogger {
     val itemView: View = View.inflate(ctx, R.layout.novel_text_page_item, null)
     var position: Int = 0
-    private val headerView: View = LayoutInflater.from(ctx).inflate(R.layout.novel_text_header, itemView as ViewGroup, false)
-    private val chapterNameTextView: TextView = headerView.chapterNameTextView
     private val textRecyclerView = itemView.textRecyclerView
     private val layoutManager: LinearLayoutManager = LinearLayoutManager(ctx)
     private val progressBar: ProgressBar = itemView.progressBar
@@ -33,13 +28,6 @@ class NovelTextViewHolder(private val ctx: NovelTextActivity, private val presen
 
     init {
         textRecyclerView.layoutManager = layoutManager
-        headerView.setOnLongClickListener {
-            refreshCurrentChapter()
-            true
-        }
-        headerView.setOnClickListener {
-            ctx.toggle()
-        }
         textRecyclerView.setOnTouchListener(object : View.OnTouchListener {
             private var previousAction: Int = MotionEvent.ACTION_UP
             @SuppressLint("ClickableViewAccessibility")
@@ -63,12 +51,10 @@ class NovelTextViewHolder(private val ctx: NovelTextActivity, private val presen
                         Settings.bottomSpacing.run { (toFloat() / 100 * ctx.window.decorView.height).toInt() })
             }
         }
-        chapterNameTextView.setTextColor(Settings.textColor)
     }
 
     fun apply(chapter: NovelChapter) {
         progressBar.show()
-        chapterNameTextView.text = chapter.name
         textListAdapter.clear()
         textListAdapter.setChapterName(chapter.name)
         presenter.attach(this)
@@ -127,7 +113,6 @@ class NovelTextViewHolder(private val ctx: NovelTextActivity, private val presen
     }
 
     fun setTextColor(color: Int) {
-        chapterNameTextView.setTextColor(color)
         textListAdapter.setTextColor(color)
     }
 
