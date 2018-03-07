8b4c64e2ea7b3ca8a9f460010be3d356e99fb6b3
AoEiuV020 <aoeiuv020@gmail.com>
2018-03-07 23:55:29 +0800

    打开地址支持小说详情页

--- app/src/main/java/cc/aoeiuv020/panovel/open/OpenManager.kt
+++ app/src/main/java/cc/aoeiuv020/panovel/open/OpenManager.kt
@@ -1,6 +1,8 @@
 package cc.aoeiuv020.panovel.open
 
 import cc.aoeiuv020.panovel.R
+import cc.aoeiuv020.panovel.api.NovelContext
+import cc.aoeiuv020.panovel.detail.NovelDetailActivity
 import cc.aoeiuv020.panovel.local.BookList
 import cc.aoeiuv020.panovel.main.MainActivity
 import cc.aoeiuv020.panovel.search.RefineSearchActivity
@@ -8,6 +10,7 @@ import cc.aoeiuv020.panovel.share.Share
 import cc.aoeiuv020.panovel.util.async
 import cc.aoeiuv020.panovel.util.loading
 import io.reactivex.Observable
+import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.error
 import java.net.MalformedURLException
 import java.net.URL
@@ -16,7 +19,7 @@ import java.net.URL
  * 应用内打开相关的，
  * Created by AoEiuV020 on 2018.03.07-21:40:28.
  */
-object OpenManager {
+object OpenManager : AnkoLogger {
     fun open(context: MainActivity, str: String) {
         try {
             val url = URL(str).toString()
@@ -29,7 +32,11 @@ object OpenManager {
 
     private fun switch(context: MainActivity, url: String) {
         context.apply {
-            if (Share.check(url)) {
+            loading(progressDialog, getString(R.string.judging))
+        }
+        if (Share.check(url)) {
+            // 如果是书单分享的地址，直接添加书单，
+            context.apply {
                 loading(progressDialog, getString(R.string.book_list_downloading))
                 Observable.fromCallable {
                     val bookList = Share.receiveBookList(url)
@@ -44,10 +51,19 @@ object OpenManager {
                     error(message, e)
                     showError(message, e)
                 })
-            } else {
-                val message = "不支持的地址或格式，"
-                showMessage(message)
             }
+        } else {
+            // 如果可以从地址得到小说对象，打开详情页，
+            Observable.fromCallable {
+                NovelContext.getNovelContextByUrl(url).getNovelItem(url)
+            }.async().subscribe({ novelItem ->
+                NovelDetailActivity.start(context, novelItem)
+                context.progressDialog.dismiss()
+            }, { e ->
+                val message = "不支持的地址或格式，"
+                error(message, e)
+                context.showMessage(message)
+            })
         }
     }
 }
