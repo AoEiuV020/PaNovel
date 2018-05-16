1af108a1054f18877624e0188e6d4d558146dd11
AoEiuV020 <aoeiuv020@gmail.com>
2018-05-16 22:49:14 +0800

    测试正则里\Q和\E之间的所有不转义

--- app/src/test/java/cc/aoeiuv020/panovel/RegexUnitTest.kt
+++ app/src/test/java/cc/aoeiuv020/panovel/RegexUnitTest.kt
@@ -1,7 +1,9 @@
 package cc.aoeiuv020.panovel
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.util.regex.Pattern
 
 /**
  * Created by AoEiuV020 on 2018.05.16-19:57:13.
@@ -18,5 +20,16 @@ class RegexUnitTest {
     @Test
     fun withoutTest() {
         assertTrue(str.matches(withoutEscapeRegex))
+    }
+
+    /**
+     * 正则里\Q和\E之间的所有不转义，
+     */
+    @Test
+    fun quote() {
+        val str = "[]"
+        val quoteStr = Pattern.quote(str)
+        assertEquals("\\Q[]\\E", quoteStr)
+        assertTrue(str.matches(Regex(quoteStr)))
     }
 }
