1dc02aed229d50395d569c2be28810a5d23f6801
AoEiuV020 <aoeiuv020@gmail.com>
2018-05-28 18:24:06 +0800

    更新日志页加个反馈途径
    也就邮箱合适了，点击跳到发邮件，

--- app/src/main/java/cc/aoeiuv020/panovel/settings/about.kt
+++ app/src/main/java/cc/aoeiuv020/panovel/settings/about.kt
@@ -8,9 +8,11 @@ import android.view.ViewGroup
 import cc.aoeiuv020.base.jar.compilePattern
 import cc.aoeiuv020.base.jar.pick
 import cc.aoeiuv020.panovel.R
+import cc.aoeiuv020.panovel.util.VersionUtil
 import kotlinx.android.synthetic.main.content_about.*
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.browse
+import org.jetbrains.anko.email
 import org.jetbrains.anko.yesButton
 
 /**
@@ -18,11 +20,15 @@ import org.jetbrains.anko.yesButton
  * Created by AoEiuV020 on 2017.11.23-10:26:44.
  */
 class AboutFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View = inflater.inflate(R.layout.content_about, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+            inflater.inflate(R.layout.content_about, container, false)
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        tvVersion.text = activity.run {
-            packageManager.getPackageInfo(packageName, 0).versionName
+        val currentVersionName = VersionUtil.getAppVersionName(activity)
+        tvVersion.text = currentVersionName
+        tvEmail.setOnClickListener {
+            email(tvEmail.text.toString(),
+                    "${activity.getString(R.string.feedback)}[${activity.getString(R.string.app_name)}]$currentVersionName")
         }
         tvChangeLog.text = activity.assets.open("ChangeLog.txt").reader().readText()
 
