7972ea7919b8dc7690a78583cbdca8233d9c6aeb
AoEiuV020 <aoeiuv020@gmail.com>
2018-06-10 20:31:13 +0800

    Site表添加了置顶时间的字段
    为了网站列表页实现置顶网站，

--- app/src/main/java/cc/aoeiuv020/panovel/data/db/AppDatabase.kt
+++ app/src/main/java/cc/aoeiuv020/panovel/data/db/AppDatabase.kt
@@ -1,10 +1,13 @@
 package cc.aoeiuv020.panovel.data.db
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.arch.persistence.room.TypeConverters
+import android.arch.persistence.room.migration.Migration
 import android.content.Context
+import android.support.annotation.VisibleForTesting
 import cc.aoeiuv020.panovel.data.dao.BookListDao
 import cc.aoeiuv020.panovel.data.dao.NovelDao
 import cc.aoeiuv020.panovel.data.dao.SiteDao
@@ -18,7 +21,7 @@ import cc.aoeiuv020.panovel.data.entity.Site
  */
 @Database(
         entities = [Novel::class, Site::class, BookListItem::class, BookList::class],
-        version = 2
+        version = 3
 )
 @TypeConverters(value = [DateTypeConverter::class])
 abstract class AppDatabase : RoomDatabase() {
@@ -33,11 +36,25 @@ abstract class AppDatabase : RoomDatabase() {
                     dbFile.path)
                     // 版本1的数据库只有网站启用设置，不迁移，略麻烦，
                     .fallbackToDestructiveMigrationFrom(1)
+                    .addMigrations(MIGRATION_2_3)
                     .build().also {
                         sInstance = it
                     }
         }
+
+        private fun migration(start: Int, end: Int, migrate: (SupportSQLiteDatabase) -> Unit) = object : Migration(start, end) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                migrate(database)
+            }
+        }
+
+        @VisibleForTesting
+        val MIGRATION_2_3 = migration(2, 3) {
+            // Site表添加了置顶时间的字段，
+            it.execSQL("ALTER TABLE Site ADD pinnedTime integer default 0 NOT NULL;")
+        }
     }
+
 
     abstract fun siteDao(): SiteDao
     abstract fun novelDao(): NovelDao
