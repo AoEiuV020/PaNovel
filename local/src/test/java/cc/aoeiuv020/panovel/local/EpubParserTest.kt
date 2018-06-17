c94201527a9b835915a188c17511f3956c0e6bba
AoEiuV020 <aoeiuv020@gmail.com>
2018-06-17 17:14:07 +0800

    测试epublib
    ide遇到问题，测试不能用，
    莫名，和之前gradle配置移到根项目build.gradle有关，

--- local/src/test/java/cc/aoeiuv020/panovel/local/EpubParserTest.kt
+++ local/src/test/java/cc/aoeiuv020/panovel/local/EpubParserTest.kt
@@ -1,13 +1,21 @@
 package cc.aoeiuv020.panovel.local
 
+import net.sf.jazzlib.ZipFile
+import nl.siegmann.epublib.epub.EpubReader
 import org.junit.Test
 
 /**
  * Created by AoEiuV020 on 2018.06.16-17:52:10.
  */
-class EpubParserTest {
+class EpubParserTest: ParserTest(EpubParser::class) {
 
     @Test
     fun parse() {
+        val file = getFile("/home/aoeiuv/tmp/panovel/epub/打工吧！魔王大人17.epub") ?: return
+        val zipFile = ZipFile(file)
+        val book =  EpubReader().readEpub(zipFile)
+        book.contents.forEach {
+            println(it)
+        }
     }
 }
