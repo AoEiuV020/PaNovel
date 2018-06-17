fc832dc5d73f2eb45884f099b6ff25b0fe9727e8
AoEiuV020 <aoeiuv020@gmail.com>
2018-06-17 13:13:58 +0800

    已经没用的两个模块忘了删除

--- RefreshRecyclerView/README.md
+++
@@ -1,5 +0,0 @@
-## https://github.com/llxdaxia/RecyclerView
-
-https://github.com/llxdaxia/RecyclerView/issues
-有点问题，id用美元$开头会找不到，
-所以直接把库复制过来，
--- RefreshRecyclerView/src/main/java/cn/lemon/view/adapter/Action.java
+++
@@ -1,9 +0,0 @@
-package cn.lemon.view.adapter;
-
-/**
- * Created by linlongxin on 2016/1/25.
- */
-@SuppressWarnings("All")
-public interface Action {
-    void onAction();
-}
