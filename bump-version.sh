3fdf0d71f51a6e5647c1f8ea8fadbe2a54e4a72e
AoEiuV020 <aoeiuv020@gmail.com>
2018-09-03 19:58:32 +0800

    版本相关配置单独一个配置文件

--- bump-version.sh
+++ bump-version.sh
@@ -6,19 +6,20 @@ old=$PWD
 cd $(dirname $0)
 project=$(pwd)
 buildGradleFile="$project/build.gradle"
+versionFile="$project/version.properties"
 
-versionCode=$(sed -n 's/\s*version_code\s*=\s*\(\S*\)/\1/p' $buildGradleFile)
+versionCode=$(sed -n 's/\s*version_code\s*=\s*\(\S*\)/\1/p' $versionFile)
 versionCode=$(expr $versionCode + 1)
 
-sed -i "s/version_code\\s*=\\s*[0-9]*/version_code = $versionCode/" $buildGradleFile
+sed -i "s/version_code\\s*=\\s*[0-9]*/version_code=$versionCode/" $versionFile
 
 versionName=$1
-sed -i "s/version_name\\s*=\\s*\".*\"/version_name = \"$versionName\"/" $buildGradleFile
+sed -i "s/version_name\\s*=\\s*.*/version_name=$versionName/" $versionFile
 changeLogFile=app/src/main/assets/ChangeLog.txt
 sed -i "1G" $changeLogFile
 sed -i "2i$versionName:" $changeLogFile
 
-git add $buildGradleFile
+git add $versionFile
 git add $changeLogFile
 git commit -m "Bumped version number to $versionName"
 
