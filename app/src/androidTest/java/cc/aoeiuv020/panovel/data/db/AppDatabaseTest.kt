104d9813379f4d2062b34150f36bde34b9ac067e
AoEiuV020 <aoeiuv020@gmail.com>
2018-12-31 19:12:01 +0800

    添加网站隐藏字段

--- app/src/androidTest/java/cc/aoeiuv020/panovel/data/db/AppDatabaseTest.kt
+++ app/src/androidTest/java/cc/aoeiuv020/panovel/data/db/AppDatabaseTest.kt
@@ -16,7 +16,6 @@ import java.util.*
  */
 @RunWith(AndroidJUnit4::class)
 class AppDatabaseTest {
-    private val TEST_DB = "migration-test"
     @Rule
     @JvmField
     val helper: MigrationTestHelper = MigrationTestHelper(InstrumentationRegistry.getInstrumentation(),
@@ -24,7 +23,7 @@ class AppDatabaseTest {
             FrameworkSQLiteOpenHelperFactory())
 
     @Test
-    fun migrationTest() {
+    fun migration23Test() {
         val site = Site("飘天文学", "https://www.piaotian.com", "https://www.piaotian.com/css/logo.gif", true, Date(0))
         val db2 = helper.createDatabase(TEST_DB, 2)
         db2.execSQL("INSERT INTO Site (name, baseUrl, logo, enabled) VALUES ('${site.name}', '${site.baseUrl}', '${site.logo}', ${if (site.enabled) 1 else 0});")
@@ -44,6 +43,33 @@ class AppDatabaseTest {
             println(result)
             assertEquals(site, result)
         }
+    }
+
+    @Test
+    fun migration34Test() {
+        val site = Site("飘天文学", "https://www.piaotian.com", "https://www.piaotian.com/css/logo.gif", true, Date(0), false)
+        val db2 = helper.createDatabase(TEST_DB, 3)
+        db2.execSQL("INSERT INTO Site (name, baseUrl, logo, enabled, pinnedTime) VALUES ('${site.name}', '${site.baseUrl}', '${site.logo}', ${if (site.enabled) 1 else 0}, ${site.pinnedTime.time});")
+        db2.close()
+        // 如果迁移后的数据库信息不匹配，这句就会报错，
+        val db3 = helper.runMigrationsAndValidate(TEST_DB, 4, true, AppDatabase.MIGRATION_3_4)
+        val c = db3.query("select * from Site;")
+        println(c.columnNames.joinToString())
+        assertEquals(1, c.count)
+        c.moveToNext()
+        val name = c.getString(0)
+        val baseUrl = c.getString(1)
+        val logo = c.getString(2)
+        val enabled = c.getInt(3) > 0
+        val pinnedTime = Date(c.getLong(4))
+        val hide = c.getInt(5) > 0
+        val result = Site(name, baseUrl, logo, enabled, pinnedTime, hide)
+        println(result)
+        assertEquals(site, result)
+    }
+
+    companion object {
+        private const val TEST_DB = "migration-test"
     }
 
 }
