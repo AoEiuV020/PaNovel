1a140f1860ad4d5b71325fec440b59849f2ddc51
AoEiuV020 <aoeiuv020@gmail.com>
2022-03-24 00:07:04 +0800

    merge自动生成commit message

--- merge-version.sh
+++ merge-version.sh
@@ -9,13 +9,16 @@ buildGradleFile="$project/build.gradle"
 versionFile="$project/version.properties"
 
 versionName=$(sed -n 's/\s*version_name\s*=\s*\(\S*\)/\1/p' $versionFile)
-
 branch=release-$versionName
+changeLog=$(./latest-changelog.sh $versionName)
+mergeLog=$(echo "Merge branch '$branch'\n\n$changeLog")
+tagLog=$(echo "$changeLog" |sed '1s/[，,]$//;1s/^\(.*\)$/\1\n/')
+
 git checkout -b $branch
 git checkout master
-git merge --no-ff $branch
+git merge --no-ff $branch -m "$mergeLog"
 git branch -d $branch
-git tag -a $versionName
+git tag -a $versionName -m "$tagLog"
 git checkout dev
 git merge master
 
