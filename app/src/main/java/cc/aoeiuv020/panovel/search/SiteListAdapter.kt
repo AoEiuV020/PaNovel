c3e332b655771f6c973df8526764ab6db91cb56c
AoEiuV020 <aoeiuv020@gmail.com>
2022-04-30 21:52:24 +0800

    书源列表中的logo支持纯文字无图片情况

--- app/src/main/java/cc/aoeiuv020/panovel/search/SiteListAdapter.kt
+++ app/src/main/java/cc/aoeiuv020/panovel/search/SiteListAdapter.kt
@@ -1,5 +1,7 @@
 package cc.aoeiuv020.panovel.search
 
+import android.graphics.Color
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +11,6 @@ import android.widget.TextView
 import cc.aoeiuv020.panovel.R
 import cc.aoeiuv020.panovel.data.DataManager
 import cc.aoeiuv020.panovel.data.entity.Site
-import cc.aoeiuv020.panovel.util.alert
 import cc.aoeiuv020.panovel.util.hide
 import cc.aoeiuv020.panovel.util.show
 import cc.aoeiuv020.panovel.util.tip
@@ -52,6 +53,7 @@ class SiteListAdapter(
         private val tvStopUpkeep: TextView = itemView.tvStopUpkeep
         private val tvName: TextView = itemView.tvName
         private val ivLogo: ImageView = itemView.ivLogo
+        private val tvLogo: TextView = itemView.tvLogo
         private val ivSettings: ImageView = itemView.ivSettings
         val cbEnabled: CheckBox = itemView.cbEnabled
         lateinit var site: Site
@@ -78,7 +80,20 @@ class SiteListAdapter(
         fun bind(data: Site) {
             this.site = data
             tvName.text = data.name
-            Glide.with(ivLogo).load(data.logo).into(ivLogo)
+            val logoUri = Uri.parse(data.logo)
+            if (logoUri.scheme == "text") {
+                tvLogo.show()
+                ivLogo.hide()
+                val foregroundColor = logoUri.getQueryParameter("fc") ?: "111111"
+                val backgroundColor = logoUri.getQueryParameter("bc") ?: "ffffff"
+                tvLogo.setBackgroundColor(Color.parseColor("#$backgroundColor"))
+                tvLogo.setTextColor(Color.parseColor("#$foregroundColor"))
+                tvLogo.text = logoUri.host
+            } else {
+                tvLogo.hide()
+                ivLogo.show()
+                Glide.with(ivLogo).load(data.logo).into(ivLogo)
+            }
             cbEnabled.isChecked = data.enabled
             val novelContext = DataManager.api.getNovelContextByName(data.name)
             if (novelContext.upkeep) {
